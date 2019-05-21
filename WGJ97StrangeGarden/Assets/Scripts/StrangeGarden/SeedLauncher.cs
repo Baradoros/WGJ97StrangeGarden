d4f61b978c296c0329a50a88171a450fd69bfc9e
deborahlow97 <deborahlow97@gmail.com>
2019-05-21 12:54:00 +0800

    Remove unused assets I created

--- WGJ97StrangeGarden/Assets/Scripts/StrangeGarden/SeedLauncher.cs
+++ WGJ97StrangeGarden/Assets/Scripts/StrangeGarden/SeedLauncher.cs
@@ -2,101 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class SeedLauncher : MonoBehaviour
 {
-    // Sound
-    private AudioSource shootSound;
-    // Touch Listener
-    public bool isTouched = false;
-    public bool isKeyPress = false;
-    // Ready for Launch
-    public bool isActive = true;
+    private Rigidbody2D rgbd;
+    private GameObject seed;
+    [SerializeField] private float force;
+    [SerializeField] private float speed;
+    [SerializeField] private float inaccuracy;
 
-    // Timers
-    private float pressTime = 0f;
-    private float startTime = 0f;
-    private int powerIndex;
-    private float force = 0f;
-    private SpringJoint2D springJoint;
-    public float maxforce = 90f;
-    public float maxPowerIndex = 8000f;
-
-    void Start()
+    // Start is called before the first frame update
+    private void Start()
     {
-        springJoint = GetComponent<SpringJoint2D>();
-        springJoint.distance = 1f;
+        seed = Resources.Load("seed") as GameObject;
+        rgbd = GetComponent<Rigidbody2D>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (isActive)
+        if(Input.GetKeyDown("space"))
         {
-            if (Input.GetKeyDown("space"))
-            {
-                isKeyPress = true;
-            }
-
-            if (Input.GetKeyUp("space"))
-            {
-                isKeyPress = false;
-            }
-
-            // keyboard press or touch hotspot
-            if (isKeyPress == true && isTouched == false || isKeyPress == false && isTouched == true)
-            {
-                if (startTime == 0f)
-                {
-                    startTime = Time.time;
-                }
-            }
-
-            // keyboard release 	
-            if (isKeyPress == false && isTouched == false && startTime != 0f)
-            {
-                Debug.Log("launch this");
-                Debug.Log(startTime);
-                if(powerIndex * maxforce <= 8000)
-                {
-                    force = powerIndex * maxforce;
-                }
-                else
-                {
-                    force = 8000;
-                }
-                Debug.Log("launched at force = " + force);
-                // reset timer & animation
-                pressTime = 0f;
-                startTime = 0f;
-                while (powerIndex >= 0)
-                {
-                    powerIndex--;
-                }
-            }
-        }
-
-        // Start Press
-        if (startTime != 0f)
-        {
-            pressTime = Time.time - startTime;
-            powerIndex = (int)(pressTime * 15);
+            ShootSeed();
         }
     }
 
-    void FixedUpdate()
+    public void ShootSeed()
     {
-        if (force != 0)
-        {
-            springJoint.distance = 1f;
-            GetComponent<Rigidbody2D>().AddForce(Vector3.up * force);
-            Debug.Log(Vector3.up * force);
-            force = 0;
-        }
+        Vector3 direction = transform.forward + Random.insideUnitSphere * inaccuracy;
 
-        if (pressTime != 0)
-        {
-            springJoint.distance = .8f;
-            GetComponent<Rigidbody2D>().AddForce(Vector3.down * 400);
-        }
+        GameObject projectile = Instantiate(seed, GetComponent<Rigidbody2D>().transform.position, Quaternion.LookRotation(direction)) as GameObject;
+        projectile.GetComponent<Rigidbody2D>().velocity = (projectile.transform.forward * speed);
+        projectile.GetComponent<Rigidbody2D>().AddForce(Vector3.up * force);
     }
 }
