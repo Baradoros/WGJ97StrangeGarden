8b52fe8578130050f026a5e195c422ba86e1800a
deborahlow97 <deborahlow97@gmail.com>
2019-05-22 21:07:08 +0800

    Fix seed launcher

---
+++ WGJ97StrangeGarden/Assets/Scripts/StrangeGarden/Scoreboard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour
+{
+    private static int playerScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void AddScore(int score)
+    {
+        playerScore += score;
+    }
+
+    public static void ResetScore()
+    {
+        playerScore = 0;
+    }
+}
--- WGJ97StrangeGarden/Assets/Scripts/StrangeGarden/SeedLauncher.cs
+++ WGJ97StrangeGarden/Assets/Scripts/StrangeGarden/SeedLauncher.cs
@@ -6,6 +6,7 @@ public class SeedLauncher : MonoBehaviour
 {
     private Rigidbody2D rgbd;
     private GameObject seed;
+    private GameObject shootingDir;
     [SerializeField] private float force;
     [SerializeField] private float speed;
     [SerializeField] private float inaccuracy;
@@ -15,12 +16,14 @@ public class SeedLauncher : MonoBehaviour
     {
         seed = Resources.Load("seed") as GameObject;
         rgbd = GetComponent<Rigidbody2D>();
+        shootingDir = transform.GetChild(0).gameObject;
+        Debug.Log(shootingDir);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("space"))
+        if(Input.GetKeyDown(KeyCode.Return))
         {
             ShootSeed();
         }
@@ -28,10 +31,13 @@ public class SeedLauncher : MonoBehaviour
 
     public void ShootSeed()
     {
+        Debug.Log(transform.forward);
         Vector3 direction = transform.forward + Random.insideUnitSphere * inaccuracy;
 
+        Vector3 forceV = (shootingDir.transform.position.normalized * force);
         GameObject projectile = Instantiate(seed, GetComponent<Rigidbody2D>().transform.position, Quaternion.LookRotation(direction)) as GameObject;
         projectile.GetComponent<Rigidbody2D>().velocity = (projectile.transform.forward * speed);
-        projectile.GetComponent<Rigidbody2D>().AddForce(Vector3.up * force);
+        Debug.Log(Vector3.up);
+        projectile.GetComponent<Rigidbody2D>().AddForce(new Vector3(0.45f, 0.45f, 0f) * force);
     }
 }
